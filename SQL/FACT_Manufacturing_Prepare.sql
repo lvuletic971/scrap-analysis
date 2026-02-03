acbf7fbc0de9483b70a8d4b49298c08b32e0d75c
lvuletic971 <lvuletic971@gmail.com>
2026-02-03 22:30:08 +0100

    Update FACT_Manufacturing_Prepare.sql

--- SQL/FACT_Manufacturing_Prepare.sql
+++ SQL/FACT_Manufacturing_Prepare.sql
@@ -1,4 +1,4 @@
---Select for scrap products
+--Separation of waste items in the overall observed period of 3 years
 
 --INSERT INTO [PrometStavkePro]
 SELECT 
@@ -15,11 +15,11 @@ FROM [Promet] pr
 LEFT JOIN PrometTransPos pos on pr.PrStPrTrans = pos.PrTpStDokumenta
 WHERE PrPrevKol >= 0 
 AND PrSifVrstePrometa = 'SKA'
-AND PrDatTrans >= '2021-10-01' AND PrDatTrans <= '2021-12-31' 
+AND PrDatTrans >= '2021-10-01' AND PrDatTrans <= '2023-12-31' 
 GROUP BY PrStZapisa, PrStDokumenta, PrDatTrans, PrSifMp, PrSifStroskMesta, PrSifVrstePrometa, PrTpVrstaNapake, PrTpVzrokNapake
 ORDER BY PrDatTrans
 
---Select for products
+--Separation of other manufactured items that are not waste in the observed period
     
 --INSERT INTO [PrometStavkeSka]
 SELECT 
@@ -34,6 +34,6 @@ FROM [Promet] pr
 LEFT JOIN PrometTransPos pos on pr.PrStPrTrans = pos.PrTpStDokumenta
 WHERE PrPrevKol >= 0 
 AND (PrSifVrstePrometa = 'PPP' OR PrSifVrstePrometa = 'PGP') 
-AND PrDatTrans >= '2021-10-01' AND PrDatTrans <= '2021-12-31' 
+AND PrDatTrans >= '2021-10-01' AND PrDatTrans <= '2023-12-31' 
 GROUP BY PrStZapisa, PrStDokumenta, PrDatTrans,PrSifMp, PrSifStroskMesta, PrSifVrstePrometa
 ORDER BY PrDatTrans
